fcd1289ff3b041c029c8f36a348f9fa192138535
siino-xyz <siino.tbh@gmail.com>
2022-03-18 15:27:08 +0900

    [fix] [id].tsxのエラーをコメントアウト

--- pages/setsunaposts/[id].tsx
+++ pages/setsunaposts/[id].tsx
@@ -1,7 +1,7 @@
 import Moment from 'react-moment'
 import { Setsunaposts } from '../../types/setsunaposts'
 import { client } from '../../libs/client'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 type Props = {
   setsunaposts: Setsunaposts
@@ -11,12 +11,12 @@ const Setsunaposts =({ setsunaposts }: Props) => {
   return (
     <div>
       <div>
-        <Image 
-          src={setsunaposts.eye_catch.url}
+        {/* <Image 
+          image={setsunaposts.eye_catch.url}
           width={900}
           height={300}
           alt="eye_catch"
-         />
+         /> */}
          <div>
            {setsunaposts.title}
          </div>
